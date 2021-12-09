c372d73d5e45c64af72cb63010d5e5a10a430574
RutvikVora <rutvikvora29@gmail.com>
2021-12-09 23:16:52 +0530

    First Submission

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#header');
+    var offset = startchange.offset();
+    $(document).scroll(function () {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $('#nav').css({ "background-color": "#73706C" });
+            $('#navlogo').css({ "height": "96px", "width": "130px" })
+        } else {
+            $('#nav').css('background-color', 'transparent');
+            $('#navlogo').css({ "height": "130px", "width": "175px" });
+        }
+    });
+
+    $("#homepage-bottom").click(function () {
+        $("#policy").addClass("d-none");
+    });
+});
