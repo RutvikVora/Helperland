23f337bc8daa6913004aa8cd96d1fbe8c9f30c21
RutvikVora <rutvikvora29@gmail.com>
2022-01-01 12:59:54 +0530

    Added Service request & User management pages

--- js/script.js
+++ js/script.js
@@ -16,8 +16,30 @@ $(document).ready(function () {
     $("#homepage-bottom").click(function () {
         $("#policy").addClass("d-none");
     });
+
+    /* adding class to tabs when it is clicked for service and admin pages */
+    $(document).on('click', '.nav-item', function () {
+        $('.menu-title').removeClass('active-tab');
+        $(this).children(".nav-link").children(".menu-title").addClass('active-tab');
+        //$(this).addClass('active-tab').siblings().removeClass('active-tab')
+    })
+
+    // Add active class to the current button (highlight it)
+    $(document).on('click', '.page-btn', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+
+    /*$(document).on('click','.nav-item',function(){
+      $('.tabcontent').addClass('active-tabcontent');
+      // Get class list string
+      var classList = $("#myDiv").attr("class");
+ 
+      // Creating class array by splitting class list string
+      var classArr = classList.split(/\s+/);
+      $.each(classList, function(index, value){ 
+          $("body").append("<p>" + index + ": " + value + "</p>");
+      });
+
+    })*/
 });
-
-
-
 
